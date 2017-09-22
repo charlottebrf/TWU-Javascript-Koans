b7af2468168674d9b4423209c2793213b303b854
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-22 15:14:17 +0100

    Refactors problem 4

--- src/4_problem.js
+++ src/4_problem.js
@@ -1,21 +1,19 @@
 function currencyCalculator(arrayOfNotes) {
-let notes = [10, 20, 50, 100, 500, 1000];
-let found = [];
-  for(i = 0; i <= arrayOfNotes.length; i++ ) {
-    if (notes.includes(arrayOfNotes[i])) {
-      found.push(arrayOfNotes[i]);
-    } else {
-      found.push(0);
+  let notes = [10, 20, 50, 100, 500, 1000];
+  let found = [];
+    for(i = 0; i <= arrayOfNotes.length; i++ ) {
+      if (notes.includes(arrayOfNotes[i])) {
+        found.push(arrayOfNotes[i]);
+      } else {
+        found.push(0);
+      }
     }
-  }
 
   let sum = 0;
   for(i = 0; i <= found.length; i++) {
-    if (found[i] == 0) { break; }
+    if (notes[i] == "0") { break; }
       sum = found.reduce((a, b) => a + b, 0);
     }
     return sum;
 }
-
-
 module.exports = currencyCalculator;
