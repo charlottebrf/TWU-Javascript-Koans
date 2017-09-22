c342430d08e101c13be3dfc9d69961e16b58ca58
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-22 15:13:55 +0100

    Refactors problem 2

--- src/2_problem.js
+++ src/2_problem.js
@@ -7,6 +7,3 @@ function trafficJamAnalysis(rickshaws, time ) {
 }
 
 module.exports = trafficJamAnalysis;
-
-
-// The Indian government traffic jam analysis department has done some research and has found that traffic jams will occur when the number of auto rickshaws on the road is more than 40. Unless it is after 8pm in which case there are no traffic jams because the daytime traffic has finished.
