27e69e2784c16b0af3918276920d59dd75646dad
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-24 13:40:52 +0100

    Minor refactor of vars to add clarity

--- src/4_problem.js
+++ src/4_problem.js
@@ -1,18 +1,17 @@
 function currencyCalculator(arrayOfNotes) {
   let notes = [10, 20, 50, 100, 500, 1000];
-  let found = [];
+  let foundNotes = [];
     for(i = 0; i <= arrayOfNotes.length; i++ ) {
       if (notes.includes(arrayOfNotes[i])) {
-        found.push(arrayOfNotes[i]);
+        foundNotes.push(arrayOfNotes[i]);
       } else {
         break;
       }
-      found;
     }
 
   let sum = 0;
-  for(i = 0; i <= found.length; i++) {
-      sum = found.reduce((a, b) => a + b, 0);
+  for(i = 0; i <= foundNotes.length; i++) {
+      sum = foundNotes.reduce((a, b) => a + b, 0);
     }
     return sum;
 }
