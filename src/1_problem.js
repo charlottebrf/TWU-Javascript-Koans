dc3ad31751e9802b68ff7827c61c65ec88c03314
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-24 14:32:40 +0100

    Solves problem 1

--- src/1_problem.js
+++ src/1_problem.js
@@ -1,5 +1,22 @@
 function kiteCompetition(number) {
-  return number;
+  result = "";
+  for(i = 1; i <= number; i++) {
+    result += phraseDecider(i);
+  }
+  return result;
+
+  function phraseDecider(i) {
+    kitePhrase = "";
+    if (i % 3 == 0 && i % 5 == 0) {
+      kitePhrase = "Ipo kaate!\n";
+    } else if (i % 5 == 0) {
+      kitePhrase = "Ipo kaate!\n";
+    } else if (i % 3 == 0) {
+      kitePhrase = "Peche!\n";
+    }
+    return kitePhrase;
+  }
+
 }
 
 module.exports = kiteCompetition;
--- test/1_problem.test.js
+++ test/1_problem.test.js
@@ -1,17 +1,18 @@
 const kiteCompetition = require('../src/1_problem');
 
 test('multiples of 3 return "Peche!"', () => {
-  expect(trafficJamAnalysis(3)).toEqual("Peche!");
+  expect(kiteCompetition(3)).toEqual("Peche!\n");
 });
 
 test('multiples of 5 return "Ipo kaate!"', () => {
-  expect(trafficJamAnalysis(5)).toEqual("Ipo kaate!");
+  expect(kiteCompetition(5)).toEqual("Peche!\nIpo kaate!\n");
 });
 
 test('multiples of 3 & 5 return "Peche! Ipo kaate!"', () => {
-  expect(trafficJamAnalysis(15)).toEqual("Peche! Ipo kaate!");
+  expect(kiteCompetition(15)).toEqual("Peche!\nIpo kaate!\nPeche!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\n");
 });
 
 test('100 prints all possibilities', () => {
-  expect(trafficJamAnalysis(100)).toEqual("xxxxx/Peche! Ipo kaate!");
-});
+  expect(kiteCompetition(100)).toEqual(
+    "Peche!\nIpo kaate!\nPeche!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\nPeche!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\nPeche!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\nPeche!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\nPeche!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\nPeche!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\nPeche!\nIpo kaate!\nPeche!\nPeche!\nIpo kaate!\n");
+  });
